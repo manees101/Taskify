6ec4623e76a5574b45d4e276c4f2944f27172921
manees101 <muhammadaneesrajpoot@gmail.com>
2024-01-20 22:45:54 +0500

    Updated the app for deployment

--- src/app.js
+++ src/app.js
@@ -4,16 +4,15 @@ const app=express();
 const path=require("path");
 const hbs=require("hbs");
 const cookieParser=require("cookie-parser");
-require("./db/conn");
 const resourcePath=path.join(__dirname,"../public");
-const viewPath=path.join(__dirname,"./templates/views");
-const partialPath=path.join(__dirname,"./templates/partials");
-const notFound=require("./middleware/not-found");
+const viewPath=path.join(__dirname,"../functions/templates/views");
+const partialPath=path.join(__dirname,"../functions/templates/partials");
+const notFound=require("../functions/middleware/not-found");
 const PORT=process.env.PORT||7000;
-const router=require("./router/router");
+const router=require("../functions/router/router");
 const bodyParser = require('body-parser');
-const errorHandler=require("./middleware/error-handler");
-const connectDB=require("./db/conn")
+const errorHandler=require("../functions/middleware/error-handler");
+const connectDB=require("../functions/db/conn")
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
